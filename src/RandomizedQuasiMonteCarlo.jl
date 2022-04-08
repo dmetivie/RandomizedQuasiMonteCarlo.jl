d4d3bc3a5c97d00bcd7c0caab6fdbe9b70e5e623
David Métivier <46794064+dmetivie@users.noreply.github.com>
2022-04-08 13:27:03 +0200

    Files generated by PkgTemplates

---
+++ src/RandomizedQuasiMonteCarlo.jl
@@ -0,0 +1,5 @@
+module RandomizedQuasiMonteCarlo
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using RandomizedQuasiMonteCarlo
+using Test
+
+@testset "RandomizedQuasiMonteCarlo.jl" begin
+    # Write your tests here.
+end
