2d3483dbec0fc0bcb5cb6c650dfae469c326de2b
David Métivier <46794064+dmetivie@users.noreply.github.com>
2022-05-20 14:16:05 +0200

    add rand method

--- src/RandomizedQuasiMonteCarlo.jl
+++ src/RandomizedQuasiMonteCarlo.jl
@@ -1,17 +1,70 @@
 module RandomizedQuasiMonteCarlo
 
 using DelimitedFiles
-using Random: rand!
 using LinearAlgebra: I, UnitLowerTriangular
+using Random
+using Distributions: Matrixvariate, Continuous
+using Random: default_rng
+import Distributions: Sampleable
+import Random: _rand!, rand!
 
+include("sampler.jl")
 include("conversion.jl")
 include("sobol.jl")
-abstract type RandomizationMethod end
+
 include("scrambling.jl")
 include("shifting.jl")
 
+# RandomizationMethod
+# struct SamplerNestedUniform{F<:AbstractArray{Bool,3},T<:AbstractArray{I,3} where {I<:Integer}} <: RandomizationMethod
+#     Bit::F
+#     Index::T
+# end
+# Sampler(RNG::Type{<:AbstractRNG}, die::RandomizationMethod, r::Random.Repetition) =
+#     SamplerSimple(die, Sampler(RNG, 1:die.nsides, r))
+
+# Sampler(RNG::Type{<:AbstractRNG}, die::RandomizationMethod, ::Val{1}) = SamplerDie1(...)
+# Sampler(RNG::Type{<:AbstractRNG}, die::RandomizationMethod, ::Val{Inf}) = SamplerDieMany(...)
+
+# rand(rng::AbstractRNG, sp::SamplerSimple{Die}) = rand(rng, sp.data)
+
+# abstract type SamplerScrambling <: Sampler{AbstractFloat} end
+# struct NestedUniformScrambler{F<:AbstractArray{Bool,3},T<:AbstractArray{I,3} where {I<:Integer}} <: SamplerScrambling
+#     Bit::F
+#     Index::T
+# end
+
+# function rand(rng::AbstractRNG, sampler::SamplerScrambling)
+#     # ... generate a single matrix sample to x
+#     x = zeros(size(sampler))
+#     random_bits = similar(sampler.Bit)
+#     scramble!(rng, x, random_bits, sampler)
+#     return x
+# end
+
+# function rand!(rng::AbstractRNG, x::DenseMatrix{T}, sampler::SamplerScrambling) where {T<:Real}
+#     # ... generate a single matrix sample to x
+#     random_bits = similar(sampler.Bit)
+#     scramble!(rng, x, random_bits, sampler)
+# end
+
+
+
+# Sampler(RNG::Type{<:AbstractRNG}, scrambling::SamplerScrambling, r::Random.Repetition) =
+#     SamplerDie(die, Sampler(RNG, 1:die.nsides, r))
+# # the `r` parameter will be explained later on
+
+# rand(rng::AbstractRNG, sp::SamplerDie) = rand(rng, sp.sp)
+
+
+# function _rand!(rng::AbstractRNG, sampler::SamplerScrambling, x::DenseMatrix{T}) where {T<:Real}
+#     # ... generate a single matrix sample to x
+#     random_bits = similar(sampler.Bit)
+#     scramble!(rng, x, random_bits, sampler)
+# end
+
 export scramble!, shift!
-export NestedUniformScrambler, LinearMatrixScrambler
+export NestedUniformScrambler, LinearMatrixScrambler, Shifter
 export sobol_pts2bits
 export which_permutation
 export nested_uniform_scramble_bit!, linear_matrix_scramble_bit!
--- src/shifting.jl
+++ src/shifting.jl
@@ -1,17 +1,48 @@
+struct Shifter{F<:AbstractMatrix{<:Real}} <: SamplerShifting{Matrixvariate,Continuous}
+    points::F
+end
 
-function shift!(x, s)
-    rand!(s)
+function prepare_shift(x)
+    return Shifter(x)
+end
+
+function shift!(rng::AbstractRNG, sampler::Shifter, x::AbstractMatrix{<:Real}, s::AbstractVector{<:Real})
+    rand!(rng, s)
     for i in 1:size(x, 1)
-        x[i, :] += s
+        x[i, :] = sampler.points[i, :] + s
     end
     frac!(x)
 end
 
-function shift!(x)
+function shift!(rng::AbstractRNG, sampler::Shifter, x::AbstractMatrix{<:Real})
     d = size(x, 2)
     s = zeros(d)
-    shift!(x, s)
+    shift!(rng, sampler, x, s)
 end
+
+function shift!(rng::AbstractRNG, sampler::Shifter, x::AbstractMatrix{<:Real})
+    d = size(x, 2)
+    s = zeros(d)
+    shift!(rng, sampler, x, s)
+end
+
+shift!(sampler::Shifter, x::AbstractMatrix{<:Real}, s::AbstractVector{<:Real}) = shift!(default_rng(), sampler, x, s)
+shift!(sampler::Shifter, x::AbstractMatrix{<:Real}) = shift!(default_rng(), sampler, x)
+
+
+# function shift!(x, s)
+#     rand!(s)
+#     for i in 1:size(x, 1)
+#         x[i, :] += s
+#     end
+#     frac!(x)
+# end
+
+# function shift!(x)
+#     d = size(x, 2)
+#     s = zeros(d)
+#     shift!(x, s)
+# end
 
 function frac!(x)
     for i in eachindex(x)
