283203127e7237768ad1cc67d6530a467caa7834
David Métivier <46794064+dmetivie@users.noreply.github.com>
2022-10-28 14:56:22 +0200

    add test from readme

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,24 @@ using RandomizedQuasiMonteCarlo
 using Test
 
 @testset "RandomizedQuasiMonteCarlo.jl" begin
-    # Write your tests here.
+    m = 7
+    N = 2^m # Number of points
+    d = 2 # dimension
+
+    u_uniform = rand(N, d) # i.i.d. uniform
+
+    unrandomized_bits = sobol_pts2bits(m, d, 32)
+    indices = which_permutation(unrandomized_bits) #32 bit version
+    random_bits = similar(unrandomized_bits) # 32 bit version
+    nus = NestedUniformScrambler(unrandomized_bits, indices)
+    lms = LinearMatrixScrambler(unrandomized_bits)
+
+    u_sob = dropdims(mapslices(bits2unif, unrandomized_bits, dims=3), dims=3)
+    u_nus = similar(u_sob)
+    u_lms = similar(u_sob)
+    u_shift = similar(u_sob)
+
+    scramble!(u_nus, random_bits, nus)
+    scramble!(u_lms, random_bits, lms)
+    shift!(u_shift)
 end
